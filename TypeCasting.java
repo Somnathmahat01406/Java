5af78b5b19e8085a67fafe8855ac3a9bbe1b0112
Somnath Mahato <somnathmahato@Somnaths-MacBook-Air.local>
2026-05-11 15:06:16 +0530

    Add TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+    public static void main(String args[]){
+        int a = 10;
+        double b = a;
+        System.out.println(a+" and "+b);
+        
+        double d = 10;
+        int c = (int)d;
+        System.out.print(d+" and "+c);
+    }
+}
