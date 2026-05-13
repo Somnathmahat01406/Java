44e27af66d1be1cb564a44843f8e5b23efe62b83
Somnath Mahato <somnathmahato@Somnaths-MacBook-Air.local>
2026-05-13 10:16:05 +0530

    Add and fix Arrya.java

---
+++ Arrya.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Arrya {
+    public static void main(String args[]){
+        int h,l,b;
+        Scanner sc = new Scanner(System.in);
+        h=sc.nextInt();
+        l=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Values entered: h=" + h + ", l=" + l + ", b=" + b);
+    }
+}
